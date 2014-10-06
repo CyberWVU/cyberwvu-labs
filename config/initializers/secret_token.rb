e3826a664f996ef63d7f5014a8f22d12ef7b9f49
loud <loud@localhost.localdomain>
2014-10-06 19:29:45 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CyberwvuLabs::Application.config.secret_key_base = '1f525fd7445ca0cad39c6e055bb548bafd8a56fe3923c18f52c49a01e1eb0fb94223ac4fa4cbe35e57a61d529ae99674a6f3d9cafa700fba590f9782f5de928a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CyberwvuLabs::Application.config.session_store :cookie_store, key: '_cyberwvu-labs_session'
