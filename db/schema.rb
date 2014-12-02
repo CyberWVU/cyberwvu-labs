767d70865d6c9ce9aa89783650bf0141dc103b64
loud <loud@localhost.localdomain>
2014-12-01 19:56:53 -0500

    admin code in views/lab_template/index.html.erb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141112231402) do
+ActiveRecord::Schema.define(version: 20141201235349) do
 
   create_table "lab_templates", force: true do |t|
     t.string   "name"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20141112231402) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
